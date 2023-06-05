36ed53d126408cc312e8aa0db909896fc10c3a0e
Raja Uppal <rajauppal@gmail.com>
2023-06-04 17:43:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+window.addEventListener('DOMContentLoaded', function () {
+  var navToggle = document.querySelector('.nav-toggle');
+  var navMenu = document.querySelector('.nav-menu');
+
+  navToggle.addEventListener('change', function () {
+    if (this.checked) {
+      navMenu.style.opacity = '1';
+      navMenu.style.pointerEvents = 'auto';
+    } else {
+      navMenu.style.opacity = '0';
+      navMenu.style.pointerEvents = 'none';
+    }
+  });
+});
